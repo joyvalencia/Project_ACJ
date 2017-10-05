813bff849af71bffdcd0b52a33fe563477961e56
Joy Castro <joly.casval@hotmail.com>
2017-10-05 21:11:57 +0200

    fantasma11

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                  }
                  echo "hola";
                  echo "Adiós";
-                 echo "hola mon"+"hola fantasma";
+                 echo "hola mon"+"hola fantasma" + "adios fantasma";
         ?> 
     </body>
 </html>
